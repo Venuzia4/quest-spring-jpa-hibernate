a22ece67c3dab8cf712540fc7d193757b174ccf4
Babongui mabika venuza <vhenuzya@yahoo.fr>
2022-10-31 18:31:38 +0100

    spring data

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,10 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-// TODO : update this interface
 
-public interface SchoolRepository {
+
+import com.wildcodeschool.wildandwizard.entity.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolRepository extends JpaRepository<School,Long>
+{
 }
